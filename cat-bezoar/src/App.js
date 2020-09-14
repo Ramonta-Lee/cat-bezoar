dbc2b6b25ab2be4d59bc6e564fa6a12b42b7574f
unknown <viola4lfe@gmail.com>
2020-09-14 10:28:48 -0400

    initial commit; initial call to axios(success), getting text;type;id

---
+++ cat-bezoar/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import FactCard from "./components/FactCard.js";
+import './App.css';
+
+function App() {
+  const [text, setText] = useState("");
+  const [type, setType] = useState("");
+  const [_id, setID] = useState(Number);
+
+  useEffect(() => {
+    axios.get(`https://cat-fact.herokuapp.com/facts`)
+    .then(response => {
+      // can access each individual object by index
+      console.log("RESPONSE.data.all",response.data.all)
+    })
+    .catch(error => console.log(error));
+  }, []);
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ cat-bezoar/src/components/FactCard.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+
+
+const FactCard = props => {
+ 
+
+
+ return (
+  <div>
+
+  </div>
+ );
+};
+
+export default FactCard;
